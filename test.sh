745bbcba701b01fb897fe8fb24a9c883920bd610
user12345df <user12345df@love.org>
2023-04-09 23:51:00 +0200

    last

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ args=$(printf '"%s",' $files)
 args=${args%?}
 echo "$args"
 
-fouine --input /home/njord/Desktop/Devs/2600/FOR/code/2600_Forensic_Tool/disks/disk.E01 --output ./Fouined
+fouine --input /home/njord/Desktop/Devs/2600/FOR/code/2600_Forensic_Tool/disks/disk.E01 --output ./Fouined --config "./configs/RegistryHiveUser.yaml","./configs/ChromeFileSystem.yaml","./configs/EdgeFileSystem.yaml","./configs/$MFT.yaml","./configs/$MFTMirr.yaml","./configs/Edge.yaml","./configs/$Boot.yaml","./configs/EventLogs.yaml","./configs/EdgeChromum.yaml","./configs/Firefox.yaml","./configs/ChromeExtensions.yaml","./configs/Chrome.yaml","./configs/RegistryHiveSystem.yaml","./configs/InternetExplorer.yaml","./configs/LogFiles.yaml","./configs/GroupPolicy.yaml","./configs/$LogFile.yaml"
